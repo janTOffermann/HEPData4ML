444b1cadead168e3ed0446cadfab3353281ccfb5
Bobkyd <106554713+Bobkyd@users.noreply.github.com>
2022-07-05 10:50:03 -0500

    Delete util_new directory

--- util_new/truth_selection.py
+++
@@ -1,9 +0,0 @@
-import numpythia as npyth # for defining selections
-
-selections = {
-    't->Wb': [
-        (npyth.STATUS == 22) & (npyth.PDG_ID == 6), # top from ttbar
-        (npyth.STATUS == 23) & (npyth.PDG_ID == 5), # bottom from t -> W b
-        (npyth.STATUS == 22) & (npyth.PDG_ID == 24) # W boson from t -> W b
-    ]
-}
