29f079a4fad532983a05e14a760995430e43ad90
Jan Offermann <offermannjan@yahoo.com>
2022-07-06 11:26:42 -0400

    removing pycache, build directories

--- util/root/vectorcalcs/build/vectorcalcs/CMakeFiles/move_header_vectorcalcs.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/move_header_vectorcalcs"
-  "../include/vectorcalcs/VectorCalcs.h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/move_header_vectorcalcs.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
