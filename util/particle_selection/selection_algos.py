85340b63cdc492465cf2cc18c2c228fbe537fa9f
Jan Tuzlic Offermann <offermannjan@yahoo.com>
2022-11-06 07:15:02 -0600

    reworking particle selection algos

--- util/particle_selection/selection_algos.py
+++ util/particle_selection/selection_algos.py
@@ -1,49 +1,56 @@
-import numpythia as npyth # for defining selections
 import numpy as np
-from util.hepmc import HepMCArrayToNumpy
-from util.particle_selection.algos import GatherQuarks, GatherStableDaughters
+from util.particle_selection.algos import *
 
-# This function just selects all actual final-state (i.e. stable) particles.
-def SelectFinalState(**kwargs):
-    event = kwargs['event']
-    selection = (npyth.STATUS == 1) & (npyth.ABS_PDG_ID > -1)
-    particles = event.all(selection=selection, return_hepmc=False)
-    status = len(particles) != 0
-    return status, particles
+# ============================================
+# Each algorithm returns a Boolean status,
+# and a list of selected particles' event listing indices.
+# These can be used by AlgoSelection() in particle_selection.py.
+# ============================================
 
-# Select the "simplest" all-quark final state -- this recursively searches the Feynman diagram starting with
-# the particles given by "truth_sel", and selects the earliest quark found along each branch.
-# This is a callable class instead of a function, so that we can configure it with its own selection!
-class SelectSimplestHadronic:
-    def __init__(self,truth_sel, return_hepmc=False):
-        self.truth_sel = truth_sel
-        self.return_hepmc = return_hepmc
+# This function just selects all actual final-state (i.e. stable) particles in an event.
+class SelectFinalState:
+    def __init__(self):
+        pass
 
-    def SetHepMC(self,flag):
-        self.return_hepmc = flag
+    def __call__(self,pythia_wrapper):
+        status = pythia_wrapper.GetStatus(hepmc=True)
+        stable = np.where(status==1)[0]
+        return (len(stable) > 0), stable
 
-    def __call__(self, event):
-        truth_arr = self.truth_sel(event,return_hepmc=True)
+# Select the "simplest" all-quark final state produced by the particles in truth_selection.
+# This recursively searches the Feynman diagram starting with the particles
+# given by "truth_sel", and selects the earliest quark found along each branch.
+class SelectSimplestQuarks:
+    def __init__(self,truth_selection):
+        self.truth_selection = truth_selection
+
+    def __call__(self,pythia_wrapper):
+        starting_particles = self.truth_selection(pythia_wrapper)
+        if(type(starting_particles) != list): starting_particles = [starting_particles]
         particles = []
-        for par in truth_arr:
-            if(par is None): return False, np.zeros(0) # part of truth_sel returned no particle -> this is bad, return status=False (failure)
-            particles += GatherQuarks(par)
-        if(not self.return_hepmc): particles = HepMCArrayToNumpy(particles)
-        return True, particles
+        for p in starting_particles:
+            gatherer = GatherQuarks()
+            particles.append(gatherer(pythia_wrapper,p))
+        if(len(particles) == 0): return False, particles
 
+        particles = np.array(particles,dtype=int).flatten()
+        return True, np.unique(particles)
+
+# Select the stable daughters of the particles chosen by truth_selection.
 class SelectFinalStateDaughters:
-    def __init__(self,truth_sel, return_hepmc=False):
-        self.truth_sel = truth_sel
-        self.return_hepmc = return_hepmc
+    def __init__(self,truth_selection):
+        self.truth_selection = truth_selection
 
-    def SetHepMC(self,flag):
-        self.return_hepmc = flag
-
-    def __call__(self, event): # TODO: Rework this.
-        truth_arr = self.truth_sel(event,return_hepmc=True)
+    def __call__(self,pythia_wrapper):
+        starting_particles = self.truth_selection(pythia_wrapper)
+        if(type(starting_particles) == int): starting_particles = [starting_particles]
+        # if(type(starting_particles) != list): starting_particles = [starting_particles]
         particles = []
-        for par in truth_arr:
-            if(par is None): return False, np.zeros(0) # part of truth_sel returned no particle -> this is bad, return status=False (failure)
-            particles += GatherStableDaughters(par)
-        if(not self.return_hepmc): particles = HepMCArrayToNumpy(particles)
-        return True, particles
+        for p in starting_particles:
+            gatherer = GatherStableDaughters()
+            daughters = gatherer(pythia_wrapper,p)
+            particles.append(daughters)
+        if(len(particles) == 0): return False, particles
+
+        particles = np.concatenate(particles,dtype=int).flatten()
+        return True, np.unique(particles)
