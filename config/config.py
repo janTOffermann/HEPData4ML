9c4b15c1aa95782cb40a4f04901249d4451b95f8
Jan Offermann <offermannjan@yahoo.com>
2023-04-25 23:13:19 -0500

    adding new post-processing scripts, updated default config for a more complex example

--- config/config.py
+++ config/config.py
@@ -1,35 +1,39 @@
+import numpy as np
 import util.jet_selection as jetsel
 import util.particle_selection.particle_selection as parsel
 import util.particle_selection.selection_algos as algos
 import util.event_selection as eventsel
 import util.event_filter as eventfilter
 import util.post_processing.tracing as tracing
+import util.post_processing.truth_sum as truth_sum
+import util.post_processing.rotate as rotate
+import util.post_processing.containment as containment
 import config.selectors as s
 
 selections = s.selections
 
 config = {
-    'proc' : 'Top_Wqq',
-    'hadronization' : True,
-    'mpi' : False,
-    'isr' : False,
-    'fsr' : False,
-    'rng' : 1, # Pythia RNG seed
+    'proc' : 'Top_Wqq', # Name must correspond to a card name (without extension) in util/pythia_templates
+    'hadronization' : True, # Pythia8 hadronization flag
+    'mpi' : False, # Pythia8 multi-parton interactions flag
+    'isr' : False, # Pythia8 initial-state radiation flag
+    'fsr' : False, # Pythia8 final-state radiation flag
+    'rng' : 1, # Pythia8 RNG seed
     'delphes' : False, # Whether or not to use Delphes
     'delphes_card' : None, # path to the Delphes card to use. If None, will use the ATLAS Delphes card that ships with Delphes
-    'delphes_dir' : None ,# directory containing the Delphes installation. If None, will be build in a local directory "delphes".
-    'fastjet_dir' : None,
-    'jet_radius': 0.8,
-    'jet_min_pt': 15., #GeV # 15
-    'jet_max_eta': 2., # absolute value eta cut # 2.
-    'jet_n_par': 200, # max number of jet constituents to save per jet
+    'delphes_dir' : None, # path to Delphes install directory. If None, will download and build Delphes locally, and use that.
+    'fastjet_dir' : None, # path to Fastjet install directory -- must have the Python extension! If None, will download and build Fastjet locally, and use that.
+    'jet_radius': 0.8, # jet radius in (eta,phi). Jets currently all use anti-kt algorithm.
+    'jet_min_pt': 15., # minimum pT cut, GeV
+    'jet_max_eta': 2., # absolute value eta cut
+    'jet_n_par': 200, # max number of jet constituents to save per jet (pT-ordered)
     'n_truth' : 3 + 2 + 120, # max number of truth particles to save per jet
-    # 'event_filter' : None,
-    'event_filter' : eventfilter.MultiFilter(
+    # 'event_filter' : None, # if not using any event filter
+    'event_filter' : eventfilter.MultiFilter( # multi-filter -- applies filters (cuts) sequentially, if an event fails a filter we generate another to replace it
         [
-            eventfilter.PtMatchedJetFilter(0.8,selections['t->Wb'],pt_window_frac=0.005,pt_min_jet=15.,eta_max_jet=2.),
-            eventfilter.ContainedJetFilter(0.8,selections['t->Wb'],selections['bqq'],matching_radius=0.6,pt_min_jet=15.,eta_max_jet=2.),
-            eventfilter.ContainedJetFilter(0.8,selections['t->Wb'],selections['t daughters'],pt_threshold_frac=0.01,pt_min_jet=15.,eta_max_jet=2.)
+            eventfilter.PtMatchedJetFilter(0.8,selections['t->Wb'],pt_window_frac=0.01,pt_min_jet=15.,eta_max_jet=2.), # require jet pT to match top pT closely
+            eventfilter.ContainedJetFilter(0.8,selections['t->Wb'],selections['bqq'],matching_radius=0.6,pt_min_jet=15.,eta_max_jet=2.), # require W(qq) & b within 0.6 of jet centroid
+            eventfilter.ContainedJetFilter(0.8,selections['t->Wb'],selections['t daughters'],pt_threshold_frac=0.01,pt_min_jet=15.,eta_max_jet=2.) # require all stable t daughters within jet, except for those which carry < 1% of total pT
         ]
     ),
     'truth_selection' : selections['t->Wb w/ qq and W daughters'],
@@ -37,17 +41,19 @@ config = {
     'event_selection' : eventsel.TruthDistanceSelection(distance=2.4, n_truth=3), # filters an event to remove some final-state particles, primarily for lowering HepMC file-size. Ideally does not affect the final output.
     'invisibles' : False, # if False, invisible particles in the final-state selection (neutrinos) will be discarded. If true, they may be input to jet clustering!
     'jet_selection':jetsel.GetNearestJet(truth_code=6,max_dr=0.8),
-    # 'jet_selection':jetsel.GetNearestJetWithContainment(6,0.8,5,-1),
     'signal_flag' : 1, # What to provide as the "signal_flag" for these events. (relevant if combining datasets). Must be >= 0.
     'post_processing': [
-        None,
-        # tracing.Tracer(verbose=False)
+        tracing.Tracer(verbose=True), # Delphes tracing, computes the "W daughteriness" of Delphes detector towers
+        truth_sum.TruthParticleSum(np.arange(5,125),0.8,verbose=True), # produce 4-vector sum of W daughters within the jet cone
+        rotate.Rotation(2,1,['Pmu'],verbose=True), # create rotated copies of 4-vectors
+        containment.Containment([3,4],jet_distance=0.8,containment_key='jet_W_contained',max_dr_key='jet_qq_dr_max',verbose=True), # compute W containment
+        containment.Containment([1,3,4],jet_distance=0.8,containment_key='jet_top_contained',max_dr_key='jet_bqq_dr_max',verbose=True), # compute top containment
     ],
     'record_final_state_indices' : True, # Whether or not to record jet constituents' indices w.r.t. the order they were passed to jet clustering (order of particles in HepMC file, or order of Delphes objects if using Delphes).
     'split_seed' : 1, # seed to be used for the RNG when splitting dataset into train/test/validation samples
     'use_vectorcalcs' : False # whether or not to use the VectorCalcs C++/ROOT library (which is part of this repo). May speed up some computations, but can lead to issues if there's a problem with the ROOT build (e.g. CVMFS/LCG_103 seems to cause issues)
 }
 
-# Don't need to adjust the lines below.
+# Don't adjust the lines below.
 try: config['truth_selection'].SetHadronization(config['hadronization'])
 except: pass
--- util/post_processing/containment.py
+++ util/post_processing/containment.py
@@ -1,29 +1,27 @@
-# The purpose of this code is to get the four-vector sum of a set of particles
-# within some distance of the jet centroid. Specifically, one gives a set of
-# truth particle indices (w.r.t. whatever truth particles have been saved in truth_Pmu),
-# and this code will sum whichever ones are within some specified dR of jet_Pmu.
-# For example, one might use this to find the four-vector sum of the daughter particles
-# of a W-boson from top quark decay, that fall within the top quark jet (the daughters will
-# have to be saved in the truth_Pmu array).
-
-import sys,os
+# The purpose of this code is to rotate four-vectors spatially, using two given
+# truth-level 4-vectors as a reference (for how to rotate things).
+# This may be useful for visualizations of the data.
+import re
 import subprocess as sub
 import numpy as np
 import h5py as h5
 from util.calcs import Calculator
+from util.post_processing.utils.rotations import DetermineRotation
 import util.qol_utils.qol_util as qu
 
-class TruthParticleSum:
-    def __init__(self,truth_indices=[],jet_distance=0.8,verbose=False):
+class Containment:
+    def __init__(self,truth_indices=[],jet_distance=0.8,containment_key='containment',max_dr_key='containment_max_dr',verbose=False):
         self.status = False
         self.SetVerbosity(verbose)
         self.SetTruthIndices(truth_indices)
         self.SetJetDistance(jet_distance)
+        self.SetContainmentKey(containment_key)
+        self.SetMaxDeltaRKey(max_dr_key)
         self.calculator = Calculator(use_vectorcalcs=False) # for now, default to false instead of having to take in the configurator
 
-        self.print_prefix = '\n\tTruthParticleSum'
+        self.print_prefix = '\n\tContainment'
         self.progress_bar_length = 50
-        self.progress_bar_prefix = '\tSumming selected truth particles within ∆R < {:.1f} of jet:'.format(self.jet_distance)
+        self.progress_bar_prefix = '\tComputing containment of selected truth particles within ∆R < {:.1f} of jet:'.format(self.jet_distance)
         self.progress_bar_suffix = 'Complete'
         self.pmu_sum = None
 
@@ -46,10 +44,19 @@ class TruthParticleSum:
         truth indices, but outside of this distance, will be omitted from the sum.
         """
         self.jet_distance = val
-        self.progress_bar_prefix = '\tSumming selected truth particles within dR = {:.1f} of jet:'.format(self.jet_distance)
+        self.progress_bar_prefix = '\tComputing containment of selected truth particles within ∆R < {:.1f} of jet:'.format(self.jet_distance)
 
-    def GetPmuSum(self):
-        return self.pmu_sum
+    def SetContainmentKey(self,key):
+        self.containment_key = key
+
+    def SetMaxDeltaRKey(self,key):
+        self.max_dr_key = key
+
+    def GetContainment(self):
+        return self.containment
+
+    def GetMaxDeltaR(self):
+        return self.max_dr
 
     def Initialize(self): # TODO: May want to consider chunking things and using a buffer? Memory usage will scale better for larger files.
         """
@@ -59,9 +66,10 @@ class TruthParticleSum:
         self.truth_Pmu = f['truth_Pmu'][:,self.truth_indices,:] # can have some zeros, if a particular event doesn't have a particle at an index in self.truth_indices
         self.truth_Nobj = f['truth_Nobj'][:]
         self.jet_Pmu_cyl = f['jet_Pmu_cyl'][:]
-        self.nevents = self.jet_Pmu_cyl.shape[0]
         self.is_signal = f['is_signal'][:] # specifically to look for negative signal flags, which indicate events that are to be discarded (and will be lacking actual jets)
-        self.pmu_sum = np.zeros((self.nevents,4),dtype=np.dtype('f8'))
+        self.nevents = self.is_signal.shape[0]
+        self.containment = np.full((self.nevents),False,dtype=bool)
+        self.max_dr = np.zeros(self.nevents,dtype=np.dtype('f8'))
         self.status = True
         f.close()
 
@@ -78,25 +86,25 @@ class TruthParticleSum:
             jet_Pmu = self.jet_Pmu_cyl[i]
             truth_Pmu_cyl = self.calculator.PxPyPzEToPtEtaPhiM(*np.roll(truth_Pmu,-1,axis=1).T)
             dr2 = self.calculator.DeltaR2Vectorized(truth_Pmu_cyl[:,1:3],np.expand_dims(jet_Pmu[1:3],0)).flatten()
-            mask = dr2 <= np.square(self.jet_distance)
-            self.pmu_sum[i,:] = np.sum(truth_Pmu[mask,:],axis=0)
+            max_dr = np.sqrt(np.max(dr2))
+            self.max_dr[i] = max_dr
+            if(max_dr < self.jet_distance): self.containment[i] = True
             if(self.verbose):
                 qu.printProgressBarColor(i+1,self.nevents,prefix=self.progress_bar_prefix,suffix=self.progress_bar_suffix,length=self.progress_bar_length)
         return
 
     # Using a generic signature -- should consider making the various post-processors inherit from a single parent class!
-    def __call__(self, delphes_file,h5_file,indices_file,output_file=None,verbose=None, copts=9, key='contained_daughter_sum_Pmu'):
+    def __call__(self, delphes_file,h5_file,indices_file,output_file=None,verbose=None, copts=9, key=None):
         self.SetH5EventFile(h5_file)
         if(verbose is not None): self.SetVerbosity(verbose)
         self.Process()
 
         if(output_file is None): output_file = h5_file
         else: sub.check_call(['cp',h5_file,output_file])
-
         if(self.verbose):
-            print('\tWriting {} to {}.'.format(key,output_file))
+            print('\tWriting {} and {} to {}.'.format(self.containment_key,self.max_dr_key, output_file))
         f = h5.File(output_file,'a')
-        d = f.create_dataset(key,data=self.GetPmuSum(),compression='gzip',compression_opts=copts)
+        d = f.create_dataset(self.containment_key,data=self.GetContainment(),compression='gzip',compression_opts=copts)
+        d = f.create_dataset(self.max_dr_key,data=self.GetMaxDeltaR(),compression='gzip',compression_opts=copts)
         f.close()
         return output_file
-
