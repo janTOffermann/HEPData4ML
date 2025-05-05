51478de087e4f20da89b57c9324d65ae02ec96d6
Jan Tuzlić Offermann <offermannjan@yahoo.com>
2025-05-05 11:01:22 -0500

    checkpointing various updates

--- condor/util/utils.py
+++ condor/util/utils.py
@@ -3,15 +3,18 @@ import subprocess as sub
 # Use this file to place functions that will otherwise clutter up prep_condor
 
 def PreparePayload(rundir,payload,gitdir='HEPData4ML'):
+
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+
     tmp_dir = 'dir_{}'.format(uuid.uuid4())
     os.makedirs(tmp_dir)
 
     # Only fetch the things we really need to copy. For example, we don't copy this condor subdir!
     paths = [
-        str(pathlib.Path('../run.py').absolute()),
-        str(pathlib.Path('../config').absolute()),
-        str(pathlib.Path('../util').absolute()),
-        str(pathlib.Path('../setup').absolute())
+        str(pathlib.Path('{}/../../run.py'.format(this_dir)).absolute()),
+        str(pathlib.Path('{}/../../config'.format(this_dir)).absolute()),
+        str(pathlib.Path('{}/../../util'.format(this_dir)).absolute()),
+        str(pathlib.Path('{}/../../setup'.format(this_dir)).absolute())
     ]
 
     gitdir_full = '{}/{}'.format(tmp_dir,gitdir)
@@ -30,7 +33,7 @@ def PreparePayload(rundir,payload,gitdir='HEPData4ML'):
         sub.check_call(command)
 
     payload_full = '{}/{}'.format(tmp_dir,payload)
-    command = ['tar','-cjf',payload,gitdir]
+    command = ['tar','-czf',payload,gitdir]
     sub.check_call(command,cwd=tmp_dir)
     command = ['rm','-r',gitdir_full]
     sub.check_call(command)
@@ -47,7 +50,7 @@ def PreparePayloadFromClone(rundir,payload,gitdir='HEPData4ML',branch='main'):
     command = ['git','clone','-b',branch,repo,gitdir]
     sub.check_call(command,cwd=tmp_dir)
     gitdir_full = '{}/{}'.format(tmp_dir,gitdir)
-    command = ['tar','-cjf',payload,gitdir]
+    command = ['tar','-czf',payload,gitdir]
     sub.check_call(command,cwd=tmp_dir)
     payload_full = '{}/{}'.format(tmp_dir,payload)
     command = ['rm','-rf',gitdir_full]
