321289be8a00e819231808265b86ba00ad76979d
Jan Tuzlić Offermann <offermannjan@yahoo.com>
2025-05-20 16:32:55 -0400

    updating ordering of keys

--- util/tools/check_file.py
+++ util/tools/check_file.py
@@ -3,6 +3,14 @@ import h5py as h5
 import numpy as np
 import argparse as ap
 
+def _sort_key(s):
+    components = s.split('.')
+    return [(components[i], i, len(components[i]), components[i])
+            for i in range(len(components))]
+
+def _key_sort(keys):
+    return sorted(keys, key=_sort_key)
+
 def _check_file(filename):
     result = pathlib.Path(filename).exists()
     if(not result):
@@ -11,10 +19,10 @@ def _check_file(filename):
 
 def _print_keys(filename):
     f = h5.File(filename,'r')
-    keys = sorted(list(f.keys()))
+    # keys = sorted(list(f.keys()))
+    keys = _key_sort(list(f.keys()))
     shapes = {k:f[k].shape for k in keys}
     max_key_length = np.max([len(k) for k in keys])
-
 
     # determine which keys correspond with metadata (attributes)
     metakeys = f.attrs.keys()
@@ -29,7 +37,8 @@ def _print_keys(filename):
     for key,val in metashapes.items():
         printkey = '[' + key + ']'
         while(len(printkey) < max_key_length + 2):
-            printkey = ' ' + printkey
+            # printkey = ' ' + printkey
+            printkey += '-'
         print('{}: {}'.format(printkey,val))
 
     print(2 * max_key_length * '-')
@@ -37,7 +46,8 @@ def _print_keys(filename):
     for key,val in shapes.items():
         printkey = '[' + key + ']'
         while(len(printkey) < max_key_length + 2):
-            printkey = ' ' + printkey
+            # printkey = ' ' + printkey
+            printkey += '-'
         print('{}: {}'.format(printkey,val))
     print(2 * max_key_length * '=')
 
@@ -59,11 +69,11 @@ def _print3d(array,lims=[]):
             print('\t[{}]: {}'.format(j,array[i,j]))
     return
 
-
 def _check_event(filename,idx=0):
     f = h5.File(filename,'r')
     metakeys = f.attrs.keys()
-    keys = [x for x in sorted(list(f.keys())) if x not in metakeys]
+    keys = [x for x in _key_sort(list(f.keys())) if x not in metakeys]
+    # keys = [x for x in sorted(list(f.keys())) if x not in metakeys]
     max_key_length = np.max([len(k) for k in keys])
 
     for key in keys:
@@ -74,21 +84,21 @@ def _check_event(filename,idx=0):
         print('{}:'.format(printkey))
         data = f[key][idx]
 
-
-
         if('Pmu' in key):
             if('Constituents' in key):
-                # try:
-                lkey1 = key.replace('Constituents.Pmu_cyl','N')
-                lkey1 = lkey1.replace('Constituents.Pmu','N')
+                try:
+                    lkey1 = key.replace('Constituents.Pmu_cyl','N')
+                    lkey1 = lkey1.replace('Constituents.Pmu','N')
 
-                lkey2 = key.replace('Pmu_cyl','N')
-                lkey2 = lkey2.replace('Pmu','N')
+                    lkey2 = key.replace('Pmu_cyl','N')
+                    lkey2 = lkey2.replace('Pmu','N')
 
-                l1 = f[lkey1][idx] + 1 # add one, to check the zero-padding
-                l2 = np.max(f[lkey2][idx] + 1) # add one, to check the zero-padding
+                    l1 = f[lkey1][idx] + 1 # add one, to check the zero-padding
+                    l2 = np.max(f[lkey2][idx] + 1) # add one, to check the zero-padding
 
-                data = f[key][idx,:l1,:l2]
+                    data = f[key][idx,:l1,:l2]
+                except:
+                    pass
 
             else:
                 try:
@@ -98,7 +108,6 @@ def _check_event(filename,idx=0):
                     data = f[key][idx,:l]
                 except:
                     pass
-
 
         if(data.ndim == 1):
             print(data)
@@ -126,7 +135,6 @@ def main(args):
 
     # Now open the file, and gather the keys.
     _print_keys(input_file)
-
 
     # Check a single event
     print('Checking first event.')
