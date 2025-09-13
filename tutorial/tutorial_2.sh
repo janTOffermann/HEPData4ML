e442dea3c2a64e05e211060d3c49ece6366390b6
Jan Tuzlić Offermann <offermannjan@yahoo.com>
2025-09-13 19:45:52 +0200

    modify tutorial_2 script to accept optional argument for number of events

--- tutorial/tutorial_2.sh
+++ tutorial/tutorial_2.sh
@@ -22,8 +22,8 @@ get_script_dir()
     echo "$SCRIPT_DIR"
 }
 
-# We'll generate 10 events.
-n=10
+# We'll generate 10 events, or however many the user requests.
+n="${1:-10}"
 
 outputDirectory=$(get_script_dir)/output/tutorial_2
 script=$(get_script_dir)/../run.py
