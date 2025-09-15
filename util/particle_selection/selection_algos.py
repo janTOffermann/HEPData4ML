5babd89e8da8b226b16a447ac37a2d53cb2bc7fa
Jan Tuzlić Offermann <jan.offermann@cern.ch>
2025-09-15 17:29:30 -0400

    working on profiling; various optimizations to truth particle selectors and coordinate conversion handling for big speed-ups

--- util/particle_selection/selection_algos.py
+++ util/particle_selection/selection_algos.py
@@ -1,6 +1,7 @@
 import numpy as np
 from util.particle_selection.algos import *
 from typing import Union, TYPE_CHECKING
+from util.misc.timing import profile_method, profile_block
 
 if TYPE_CHECKING: # Only imported during type checking -- avoids risk of circular imports
     from util.hepmc.setup import HepMCSetup, uncache_hepmc3, prepend_to_pythonpath
@@ -22,87 +23,69 @@ if TYPE_CHECKING: # Only imported during type checking -- avoids risk of circula
 # ============================================
 
 class BaseSelectorAlgorithm:
-    """
-    Base selection algorithm class.
-    Consider moving some stuff here.
-    """
+    """Keep simple"""
     def __init__(self):
         pass
 
-# This function just selects all actual final-state (i.e. stable) particles in an event.
 class SelectFinalState(BaseSelectorAlgorithm):
+    """Minimal optimization: vectorize the status check"""
     def __init__(self):
         pass
 
-    def __call__(self,hepev:'hm.GenEvent'):
-        status = [x.status() for x in hepev.particles()]
-        stable = np.where(status==1)[0]
-        return (len(stable) > 0), stable
+    @profile_method('SelectFinalState.__call__')
+    def __call__(self, hepev: 'hm.GenEvent') -> tuple:
+        particles = hepev.particles()
+        status_array = np.array([p.status() for p in particles], dtype=np.int32)
+        stable_indices = np.where(status_array == 1)[0]
+        return len(stable_indices) > 0, stable_indices
 
-# Select the immediate daughters of some particle.
 class SelectDaughters(BaseSelectorAlgorithm):
-    """
-    Select the immediate daughter(s) of some particle, in event listing.
-    Does not include radiation like X -> X gamma (here, {X,gamma} are *not*
-    daughters, we look further at how X decays to find them).
-    """
-    def __init__(self,truth_selection):
+    """Keep original structure but use optimized gatherer"""
+    def __init__(self, truth_selection):
         self.truth_selection = truth_selection
         self.gatherer = GatherDaughters()
 
-    def __call__(self,hepev:'hm.GenEvent'):
+    @profile_method('SelectDaughters.__call__')
+    def __call__(self, hepev: 'hm.GenEvent') -> tuple:
         starting_particles = self.truth_selection(hepev)
-        if(type(starting_particles) != list): starting_particles = [starting_particles]
-        particles = []
+        if not isinstance(starting_particles, (list, np.ndarray)):
+            starting_particles = [starting_particles]
+        
+        all_daughters = []
         for p in starting_particles:
-            particles.append(self.gatherer(hepev,p))
-        if(len(particles) == 0): return False, particles
+            daughters = self.gatherer(hepev, p)
+            all_daughters.extend(daughters)
+        
+        if not all_daughters:
+            return False, np.array([], dtype=np.int32)
 
-        particles = np.array(particles,dtype=int).flatten()
-        return True, np.unique(particles)
+        return True, np.unique(np.array(all_daughters, dtype=np.int32))
 
-# class SelectSimplestQuarks(SelectDaughters):
-#     """
-#     Select the "simplest" all-quark final state produced by the particles in truth_selection.
-#     This recursively searches the Feynman diagram starting with the particles
-#     given by "truth_sel", and selects the earliest quark found along each branch.
-#     """
-#     def __init__(self,truth_selection):
-#         self.truth_selection = truth_selection
-#         self.gatherer = GatherQuarks()
-
-# # Select the "simplest" all-quark final state produced by the particles in truth_selection.
-# # This recursively searches the Feynman diagram starting with the particles
-# # given by "truth_sel", and selects the earliest quark found along each branch.
-# class SelectSimplestQuarks:
-#     def __init__(self,truth_selection):
-#         self.truth_selection = truth_selection
-
-#     def __call__(self,pythia_wrapper):
-#         starting_particles = self.truth_selection(pythia_wrapper)
-#         if(type(starting_particles) != list): starting_particles = [starting_particles]
-#         particles = []
-#         for p in starting_particles:
-#             gatherer = GatherQuarks()
-#             particles.append(gatherer(pythia_wrapper,p))
-#         if(len(particles) == 0): return False, particles
-#         particles = np.array(particles,dtype=int).flatten()
-#         return True, np.unique(particles)
-
-# Select the stable daughters of the particles chosen by truth_selection.
 class SelectFinalStateDaughters(BaseSelectorAlgorithm):
-    def __init__(self,truth_selection):
+    """Use the optimized gatherer but keep simple interface"""
+    def __init__(self, truth_selection):
         self.truth_selection = truth_selection
+        self.gatherer = GatherStableDaughters()
 
-    def __call__(self,hepev:'hm.GenEvent'):
+    @profile_method('SelectFinalStateDaughters.__call__')
+    def __call__(self, hepev: 'hm.GenEvent') -> tuple:
         starting_particles = self.truth_selection(hepev)
-        if(type(starting_particles) not in [list,np.ndarray]): starting_particles = [starting_particles]
-        particles = []
-        for i,p in enumerate(starting_particles):
-            gatherer = GatherStableDaughters()
-            daughters = gatherer(hepev,p)
-            particles.append(daughters)
-        if(len(particles) == 0): return False, particles
-
-        particles = np.unique(np.concatenate(particles,dtype=int).flatten())
-        return True, particles
+        if not isinstance(starting_particles, (list, np.ndarray)):
+            starting_particles = [starting_particles]
+        
+        if not starting_particles or starting_particles[0] is None:
+            return False, np.array([], dtype=np.int32)
+        
+        all_daughters = []
+        for start_idx in starting_particles:
+            if start_idx is None or start_idx < 0:
+                continue
+            daughters = self.gatherer(hepev, start_idx)
+            all_daughters.extend(daughters)
+        
+        if not all_daughters:
+            return False, np.array([], dtype=np.int32)
+        
+        # Use numpy unique for deduplication and sorting
+        result = np.unique(np.array(all_daughters, dtype=np.int32))
+        return True, result
