basic fixes for rho-area subtraction; adding computation of jet areas (off by default; can be turned on by post-processors)

util/fastjet/jetfinderbase.py
@@ -28,7 +28,12 @@ class JetFinderBase:
         self.jet_algorithm = None
         self.jetdef = None
         self.cluster_sequence = None
-        # self.jets = None
+
+        # for jet areas (which certain post-processors might need)
+        self.use_area = False
+        self.area_def = None
+        self.area_ghost_max_rapidity = 5.
+
         self.jets_dict = None
         self.jet_vectors = None
         self.jet_vectors_cyl = None
@@ -100,6 +105,13 @@ class JetFinderBase:
     def SetRapidity(self,val):
         self.rapidity = val
 
+    def SetUseArea(self,val:bool):
+        import fastjet as fj
+        self.use_area = val
+
+    def SetAreaGhostMaxRapidity(self,val:float):
+        self.area_ghost_max_rapidity = val
+
     def _setupFastJet(self):
         verbose = self.configurator.GetPrintFastjet()
         self.fastjet_setup = FastJetSetup(self.configurator.GetFastjetDirectory(),full_setup=True,verbose=verbose)
@@ -146,6 +158,9 @@ class JetFinderBase:
         self._initialize_fastjet()
         import fastjet as fj # hacky, but will work because _setupFastJet() was run in __init__()
 
+        if(self.use_area and self.area_def is None):
+            self.area_def = fj.AreaDefinition(fj.active_area,fj.GhostedAreaSpec(self.area_ghost_max_rapidity))
+
         # Quick check to make sure our PseudoJet buffer is large enough.
         # Ideally, we should prepare this upstream to avoid having to re-initialize.
         n_pseudojets = len(self.input_vecs)
@@ -174,8 +189,14 @@ class JetFinderBase:
                 self.pseudojets[idx].set_python_info(val)
 
         with profile_block('JetFinderBase._clusterJets - ClusterSequence'): # Useful for profiling -- this time block is largely non-negotiable.
-            self.cluster_sequence = fj.ClusterSequence(self.pseudojets[:n_pseudojets], self.jetdef) # member of class, otherwise goes out-of-scope when ref'd later
-        self.jets_dict = {i:jet for i,jet in enumerate(self.cluster_sequence.inclusive_jets())} # NOTE: Repeated calls to ClusterSequence::inclusive_jets() seems OK, I think it is just an accessor.
+
+            if(not self.use_area):
+                self.cluster_sequence = fj.ClusterSequence(self.pseudojets[:n_pseudojets], self.jetdef) # member of class, otherwise goes out-of-scope when ref'd later
+            else:
+                self.cluster_sequence = fj.ClusterSequenceArea(self.pseudojets[:n_pseudojets], self.jetdef, self.area_def) # member of class, otherwise goes out-of-scope when ref'd later
+            self.jets_dict = {i:jet for i,jet in enumerate(self.cluster_sequence.inclusive_jets())} # NOTE: Repeated calls to ClusterSequence::inclusive_jets() seems OK, I think it is just an accessor.
+
+
         self.jet_ordering = np.arange(len(self.jets_dict))
         self.pt_sorting = self.jet_ordering
         self._jetsToVectors()

util/reconstruction/post_processing/utils/rho_area_subtraction.py
@@ -73,6 +73,8 @@ class RhoAreaSubtraction(PostProcessorBase):
         self.obj_name_output = '.'.join([self.obj_name_input,'RhoPileupSubtracted'])
         self.obj_name_constituents_output = self.obj_name_output
 
+        obj.SetUseArea(True) # will need jet areas
+
         if(self.rho_input is not None):
             self._set_rho_input(obj)
             self.compute = False
@@ -126,29 +128,25 @@ class RhoAreaSubtraction(PostProcessorBase):
                 area = jet.area_4vector() # pseudojet
 
             if(area is None):
-                self._print('[{}] No area'.format(i))
                 continue
 
             # determine the appropriate value of rho to use, based on jet's location
             rho = None
-            rho_idx = np.searchsorted(self.rho_eta_edges, pmu_cyl[1], side='right') - 1
+            rho_idx = np.searchsorted(self.rho_eta_edges[:,0], pmu_cyl[1], side='right') - 1
             if(0 <= rho_idx < len(self.rho)):
                 rho = self.rho[rho_idx]
 
             if(rho is None):
-                self._print('[{}] No rho'.format(i))
                 continue
 
             rho_area = rho * area # Fastjet::PseudoJet
 
             if(rho_area.pt() >= pmu_cyl[0]):
-                self._print('[{}] rho_area.pt() >= jet pt'.format(i))
                 delete_indices.append(i)
                 continue
 
             corr_jet = jet - rho_area
             if(corr_jet.pt() < self.pt_min):
-                self._print('[{}] corr_jet.pt() = {}, < self.pt_min'.format(i,corr_jet.pt()))
                 delete_indices.append(i)
                 continue
             self.pmu[i]     = np.array([corr_jet.e(),corr_jet.px(),corr_jet.py(),corr_jet.pz()])